9986836f67ad1208a8410b847c8826099b32f375
Devin Droddy <devin.droddy@gmail.com>
2024-08-07 17:45:14 -0400

    add disabled option to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,20 +4,23 @@ export default function Button({
   title,
   onPress,
   textSize = 20,
+  disabled,
 }: {
   title: string;
   onPress?: () => void | Promise<void>;
   textSize?: number;
+  disabled?: boolean;
 }) {
   return (
     <Pressable
       onPress={onPress}
       style={{
-        backgroundColor: '#0101FF',
+        backgroundColor: disabled ? '#CCCCCC' : '#0101FF',
         borderRadius: 4,
         padding: 8,
         margin: 4,
       }}
+      disabled={disabled}
     >
       <Text style={{ fontSize: textSize, color: 'white' }}>{title}</Text>
     </Pressable>
