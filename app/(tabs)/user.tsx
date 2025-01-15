60565c2b8c2cb78b3d3d2344a7321516bc86aec6
David Droddy <david.droddy@gmail.com>
2025-01-15 17:22:55 -0500

    feat: removes apple and google logins

--- app/(tabs)/user.tsx
+++ app/(tabs)/user.tsx
@@ -8,23 +8,12 @@ import Toast, {
   ToastConfig,
 } from 'react-native-toast-message';
 import {
-  GoogleAuthProvider,
-  OAuthProvider,
-  signInWithCredential,
   User as FireUser,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from 'firebase/auth';
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-} from '@react-native-google-signin/google-signin';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
-
-import * as Apple from 'expo-apple-authentication';
-
 import { User as UserInfo } from '@/types/firestore';
-
 import { i18nContext } from '@/i18n';
 import { styles } from '@/constants/style';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -35,8 +24,6 @@ export default function User() {
   const [fireUser, setFireUser] = useState<FireUser | undefined>(
     auth.currentUser as FireUser
   );
-  const [appleUser, setAppleUser] =
-    useState<Apple.AppleAuthenticationCredential>();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -100,8 +87,6 @@ export default function User() {
     ),
   };
 
-  useEffect(GoogleSignin.configure, []);
-
   useEffect(() => {
     const effect = async () => {
       if (fireUser) {
@@ -109,62 +94,13 @@ export default function User() {
           await getDoc(doc(db, 'people', fireUser.uid))
         ).data() as UserInfo | undefined;
         if (!userInfo) {
-          let lastName;
-          let firstName;
-
-          const googleUser = GoogleSignin.getCurrentUser();
-          if (googleUser) {
-            const googleUserInfo = googleUser.user;
-            lastName = googleUserInfo?.familyName;
-            firstName = googleUserInfo?.givenName;
-          } else if (appleUser) {
-            firstName = appleUser?.fullName?.givenName;
-            lastName = appleUser?.fullName?.familyName;
-          }
-
-          userInfo = {
-            firstName: firstName ?? '',
-            lastName: lastName ?? '',
-            role: '',
-          };
           setDoc(doc(db, 'people', fireUser.uid), userInfo);
         }
-
         setUserInfo(userInfo);
       }
     };
     effect();
   }, [fireUser]);
-
-  const signInGoogle = async () => {
-    await GoogleSignin.hasPlayServices();
-    await GoogleSignin.signIn();
-    const tokens = await GoogleSignin.getTokens();
-    const cred = GoogleAuthProvider.credential(
-      tokens.idToken,
-      tokens.accessToken
-    );
-    const res = await signInWithCredential(auth, cred);
-    setFireUser(res.user);
-  };
-
-  const signInApple = async () => {
-    const appleCred = await Apple.signInAsync({
-      requestedScopes: [
-        Apple.AppleAuthenticationScope.EMAIL,
-        Apple.AppleAuthenticationScope.FULL_NAME,
-      ],
-    });
-    let idToken = appleCred.identityToken ? appleCred.identityToken : '';
-    let accessToken = appleCred.authorizationCode ?? '';
-    setAppleUser(appleCred);
-    const firebaseCred = new OAuthProvider('apple.com').credential({
-      idToken,
-      accessToken,
-    });
-    const res = await signInWithCredential(auth, firebaseCred);
-    setFireUser(res.user);
-  };
 
   const validateEmail = (email: string) => {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -205,7 +141,9 @@ export default function User() {
           lastName,
           role: '',
         };
+
         await setDoc(doc(db, 'people', res.user.uid), userInfo);
+
         setUserInfo(userInfo);
         setFireUser(res.user);
       } else {
@@ -229,9 +167,6 @@ export default function User() {
     auth.signOut();
     setFireUser(undefined);
     setUserInfo(undefined);
-    if (GoogleSignin.getCurrentUser()) {
-      GoogleSignin.signOut();
-    }
   };
 
   return (
@@ -245,22 +180,9 @@ export default function User() {
           <Button title={translate('signOut')} onPress={signOut} />
         </>
       )}
-      {fireUser && !userInfo && <ActivityIndicator />}
+      {!userInfo && fireUser && <ActivityIndicator />}
       {!fireUser && (
         <>
-          <GoogleSigninButton
-            size={GoogleSigninButton.Size.Wide}
-            color={GoogleSigninButton.Color.Dark}
-            onPress={signInGoogle}
-          />
-          {/* Will not render if apple authentication isn't available :D */}
-          <Apple.AppleAuthenticationButton
-            buttonType={Apple.AppleAuthenticationButtonType.SIGN_IN}
-            style={{ width: 200, height: 44 }}
-            buttonStyle={Apple.AppleAuthenticationButtonStyle.BLACK}
-            onPress={signInApple}
-          />
-          <Text style={styles.text}>{`${translate('or')}`}</Text>
           {isSignUp && (
             <>
               <TextInput
