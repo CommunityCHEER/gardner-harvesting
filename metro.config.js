0383e52025fa1f78425c46f4551913edfbe30cdb
David Droddy <David.Droddy@pm.me>
2026-02-28 14:43:26 -0500

    config: disable unstable_enablePackageExports in metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,5 @@ const { getDefaultConfig } = require("expo/metro-config");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 config.resolver.assetExts.push("cjs");
-config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
